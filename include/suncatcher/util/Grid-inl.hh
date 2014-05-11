827a0bc5a8fec96a30203aa4fe3075170cfaa2b1
Alex Roper <alex@aroper.net>
2014-05-11 12:15:57 -0400

    Prepare merge.

--- include/suncatcher/util/Grid-inl.hh
+++ include/suncatcher/util/Grid-inl.hh
@@ -18,7 +18,7 @@
 #ifndef GRID_INL_93b27f51b9f84797b7b0b059118d798a
 #define GRID_INL_93b27f51b9f84797b7b0b059118d798a
 
-#include "suncatcher/util/util.hh"
+#include <type_traits>
 
 namespace suncatcher {
 namespace util {
@@ -26,35 +26,90 @@ namespace util {
 template <typename T>
 inline Grid<T>::Grid()
 : my_size({0, 0, 0}),
-  backing() { }
+  length(0),
+  backing(nullptr) { }
 
 
 template <typename T>
 inline Grid<T>::Grid(const Coord size_in, const T& val)
 : my_size(size_in),
-  backing(size_in.row * size_in.col * size_in.layer, val) { }
+  length(size_in.row * size_in.col * size_in.layer),
+  backing(new T[size_in.row * size_in.col * size_in.layer]) {
+
+  fill(val);
+}
+
+
+template <typename T>
+inline Grid<T>::Grid(uint16_t r, uint16_t c, uint16_t l, const T& val)
+  : Grid({r, c, l}, val) { }
+
+
+
+template <typename T>
+inline Grid<T>& Grid<T>::operator=(const Grid<T>& other) {
+  my_size = other.my_size;
+  if (length != other.length) {
+    length = other.length;
+    backing.reset(new T[other.length]);
+  }
+
+  for (size_t i = 0; i < length; ++i) {
+    backing[i] = other.backing[i];
+  }
+  return *this;
+}
+
+
+template <typename T>
+inline Grid<T>::Grid(const Grid<T>& other)
+: my_size(other.my_size),
+  length(other.length),
+  backing(new T[other.length]) {
+
+  for (size_t i = 0; i < length; ++i) {
+    backing[i] = other.backing[i];
+  }
+}
 
 
 template <typename T>
 inline void Grid<T>::fill(const T& val) {
-  std::fill(backing.begin(), backing.end(), val);
+  for (size_t i = 0; i < length; ++i) {
+    backing[i] = val;
+  }
 }
 
 
 template <typename T>
 inline T& Grid<T>::at(const Coord cell) {
-  assert(check_bounds(cell));
-  return backing[cell.col + my_size.col * (cell.row + my_size.row * cell.layer)];
+  check_bounds(cell);
+  size_t index = cell.col + my_size.col * (cell.row + my_size.row * cell.layer);
+  return backing[index];
 }
 
 
 template <typename T>
 inline const T& Grid<T>::at(const Coord cell) const {
-  assert(check_bounds(cell));
-  return backing[cell.col + my_size.col * (cell.row + my_size.row * cell.layer)];
+  check_bounds(cell);
+  size_t index = cell.col + my_size.col * (cell.row + my_size.row * cell.layer);
+  return backing[index];
 }
 
 
+template <typename T>
+inline const T& Grid<T>::at(uint16_t row, uint16_t col, uint16_t layer) const {
+  return at({row, col, layer});
+}
+
+
+template <typename T>
+inline T& Grid<T>::at(uint16_t row, uint16_t col, uint16_t layer) {
+  return at({row, col, layer});
+}
+
+
+//TODO: evaluate cache performance of ordering.
 template <typename T>
 inline void Grid<T>::get_adjacent(
     const Coord cell,
@@ -62,13 +117,55 @@ inline void Grid<T>::get_adjacent(
     std::vector<Coord>& neighbors
     ) const {
 
-  util::get_adjacent(cell, include_diagonals, neighbors, my_size);
+  neighbors.clear();
+  neighbors.reserve(10);
+
+  static_assert(
+      std::is_same<decltype(Coord::row), decltype(Coord::col)>::value,
+      ""
+    );
+  static_assert(
+      std::is_same<decltype(Coord::row), decltype(Coord::layer)>::value,
+      ""
+    );
+
+  const static decltype(Coord::row) n1(-1);
+
+  const static std::vector<Coord> ADJ_DELTA{
+    // Manhattan adjacent same layer.
+    {0, n1, 0}, {n1, 0, 0}, {0, 1, 0}, {1, 0, 0},
+
+    // Manhattan adjacent vertical and horizontal.
+    {0, 0, 1}, {0, 0, n1},
+
+    // Same layer diagonal.
+    {1, 1, 0}, {n1, 1, 0}, {1, n1, 0}, {n1, n1, 0}
+  };
+
+
+  for (size_t i = 0; i < (size_t)(include_diagonals ? 10 : 6); ++i) {
+    auto n = ADJ_DELTA[i] + cell;
+    if (check_bounds(n)) {
+      neighbors.push_back(n);
+    }
+  }
 }
 
 
 template <typename T>
 inline bool Grid<T>::check_bounds(const Coord cell) const {
-  return util::check_bounds(cell, my_size);
+  return (cell.row < my_size.row && cell.col < my_size.col && cell.layer < my_size.layer);
+}
+
+
+template <typename T>
+inline bool Grid<T>::check_bounds(
+    uint16_t row,
+    uint16_t col,
+    uint16_t layer
+  ) const {
+
+  return check_bounds({row, col, layer});
 }
 
 
@@ -80,7 +177,15 @@ inline const Coord& Grid<T>::size() const {
 
 template <typename T>
 inline bool Grid<T>::operator==(const Grid<T>& other) const {
-  return (other.size() == size() && other.backing == backing);
+  if (other.size() != size()) {
+    return false;
+  }
+  for (size_t i = 0; i < length; ++i) {
+    if (other.backing[i] != backing[i]) {
+      return false;
+    }
+  }
+  return true;
 }
 
 
